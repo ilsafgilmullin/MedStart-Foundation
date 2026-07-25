8a7c8bf00c7f70c9cd7934593de86219a623e351
ilsafgilmullin <ilsafgilmullin@gmail.com>
2026-07-25 11:43:02 +0300

    chore: remove generated artifacts and obsolete mockup workspace

--- artifacts/medstart/.next/static/chunks/pages/_app-f420b0a8d7af1ba6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{9843:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(1615)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(9843),n(4455))),_N_E=_.O()}]);
